2c364bb031fdce957b48335877eda377cc31c247
maudg00 <70607519+maudg00@users.noreply.github.com>
2020-10-28 15:55:28 -0600

    Add files via upload

---
+++ EjercicioFinal (1).cpp
@@ -0,0 +1,42 @@
+#include "mbed.h"
+Serial pc(USBTX, USBRX);
+Ticker ticker;
+InterruptIn interrupt(PTA1);
+DigitalIn botonIncrementarEspera(PTA2);
+DigitalIn botonPrenderLED(PTD4);
+DigitalOut Led1(PTB0);
+DigitalOut Led2(PTB1);
+Timer t;
+Timeout timeout;
+void ensenarTiempo()
+{
+    pc.printf("El tiempo transcurrido es %f.\n", t.read());
+}
+void prenderLED1()
+{
+    Led1=1;
+}
+void prenderLED2()
+{
+    Led2=1;
+    wait(0.5);
+    Led2=0;
+}
+int main() {
+    int espera=1;
+    t.start();
+    interrupt.rise(&ensenarTiempo);
+    ticker.attach(&prenderLED2, 1);
+    while(1)
+     {
+        if(botonIncrementarEspera==1)
+        {
+            espera++;
+        }
+        if(botonPrenderLED==1)
+        {
+            timeout.attach(&prenderLED1, espera);
+        }
+        wait(0.2);
+    }
+}
